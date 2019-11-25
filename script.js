08500c023c9f18b5ac10fa74e24319c8c77cd696
Vlad Savchenko <polkamarka@gmail.com>
2019-11-25 16:38:32 +0200

    Start of work

--- script.js
+++ script.js
@@ -94,3 +94,22 @@ $backButtonPractice.click(function(event){
         $secondScreen.css('display', 'flex');
     }
 });
+
+$(document).ready(function(){
+        //Скрыть PopUp при загрузке страницы    
+        PopUpHide();
+    });
+    //Функция отображения PopUp
+    function PopUpShow(){
+        $("#popup1").show();
+    }
+    //Функция скрытия PopUp
+    function PopUpHide(){
+        $("#popup1").hide();
+    }
+    
+$(".classtype__button-choise").click(function(event){
+    event.preventDefault();
+    PopUpShow();
+});
+
